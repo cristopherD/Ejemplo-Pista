a6f3094b213e49cc6832dad6eacbbe3935dc0b82
cristhoper <cristhoper@lenovo>
2016-02-02 13:23:25 +0100

    ejmplo-pista

---
+++ ejemplo-pista/calificar.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$p1 = $_REQUEST['p1'];
+$p2 = $_REQUEST['p2'];
+$p3 = $_REQUEST['p3'];
+$p4 = $_REQUEST['p4'];
+echo $p1," - ",$p2," - ",$p3," - ",$p4;
+
+$usuario = $_REQUEST['usuario'];
---
+++ ejemplo-pista/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <h3>Aplicación que valide a un usuario, le presente un examen de tipo test, el usuario conteste a las preguntas,
+            el sistema corrija y puntúe y guarde el examen.</h3>
+        <form action="validalogin.php" method="post">
+Usuario: <input type="text" name="usuario"><br>
+Contaseña: <input type="password" name="pass">
+<input type="submit">
+    </body>
+</html>
